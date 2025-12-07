00b97e1e3edef5013d9bceb67c216a5b786c4e4c
FelipeSanAraujo <fsaraujo1230@gmail.com>
2025-12-07 02:15:41 -0300

    Adicionando a lógica

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("\nSmartphone Nokia: Lumia 1020");
+Nokia nokia = new Nokia("123456789", "Lumia 1020", "IMEI123456789", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+System.Console.WriteLine("\nSmartphone iPhone: iPhone 13");
+Iphone iphone = new Iphone("987654321", "iPhone 13", "IMEI987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
